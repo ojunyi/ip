3d0caca17403c3b7236c22f8cc28f5f1300be1b5
JY <junyi4446@gmail.com>
2026-01-22 10:08:16 +0800

    Added: Greetings and goodbye

---
+++ src/main/java/BuddiBoi.java
@@ -0,0 +1,14 @@
+public class BuddiBoi {
+    public static void main(String[] args) {
+        String logo = "______           _     _ _______       _ \n"
+                    + "| ___ \\         | |   | (_) ___ \\     (_)\n"
+                    + "| |_/ /_   _  __| | __| |_| |_/ / ___  _ \n"
+                    + "| ___ \\ | | |/ _` |/ _` | | ___ \\/ _ \\| |\n"
+                    + "| |_/ / |_| | (_| | (_| | | |_/ / (_) | |\n"
+                    + "\\____/ \\__,_|\\__,_|\\__,_|_\\____/ \\___/|_|\n";
+
+        System.out.println("Hello! I am\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
