8b2fc89bdd514cc054a617289ea8ea4c659bb51a
JY <junyi4446@gmail.com>
2026-01-22 11:18:17 +0800

    Added: Mark as Done + Task.java class

--- src/main/java/BuddiBoi.java
+++ src/main/java/BuddiBoi.java
@@ -13,11 +13,11 @@ public class BuddiBoi {
         
         
         Scanner sc = new Scanner(System.in);
-        String[] list = new String[100]; // No more than 100 items
+        Task[] tasks = new Task[100]; // No more than 100 Tasks
         int itemCount = 0;
 
         System.out.println(divider);
-        System.out.println(indent + "Hiiii! I am\n" + logo);
+        System.out.println(indent + "Hey, whats up! I am\n" + logo);
         System.out.println(indent + "What can I do for you today?");
         System.out.println("\n" + divider);
 
@@ -27,21 +27,36 @@ public class BuddiBoi {
 
             if (input.equals("bye")) {
                 System.out.println(divider);
-                System.out.println(indent + "See you next timeee!");
+                System.out.println(indent + "See you next timeee! Ciaoooo!");
                 System.out.println("\n" + divider);
                 break;
             } else if (input.equals("list")) {
                 System.out.println(divider);
+                System.out.println(indent + "Here's your tasks lists:");
                 for (int i = 0; i < itemCount; i++) {
-                    System.out.println(indent + (i + 1) + ". " + list[i]);
+                    System.out.println(indent + (i + 1) + ". " + tasks[i].getStatus() + " " + tasks[i].description()    );
                 }
+                System.out.println("\n" + divider);
+            } else if (input.startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(input.substring(5)) - 1;
+                tasks[taskNumber].markAsDone();
+                System.out.println(divider);
+                System.out.println(indent + "Alright! Done that task shall be:");
+                System.out.println(indent + "  " + tasks[taskNumber].getStatus() + " " + tasks[taskNumber].description());
+                System.out.println("\n" + divider);
+            } else if (input.startsWith("unmark ")) {
+                int taskNumber = Integer.parseInt(input.substring(7)) - 1;
+                tasks[taskNumber].markAsNotDone();
+                System.out.println(divider);
+                System.out.println(indent + "OK. Task shall be undone:");
+                System.out.println(indent + "  " + tasks[taskNumber].getStatus() + " " + tasks[taskNumber].description());
                 System.out.println("\n" + divider);
             } else {
                 System.out.println(divider);
                 System.out.println(indent + "Added: " + input);
                 System.out.println("\n" + divider);
 
-                list[itemCount] = input;
+                tasks[itemCount] = new Task(input);
                 itemCount++;
             }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+/** 
+ **Taken from "https://nus-cs2103-ay2526-s2.github.io/website/schedule/week2/project.html"
+ Level 3 Extension: A-Classes
+ **/
+public class Task { 
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatus() {
+        return (this.isDone ? "[X]" : "[ ]");
+    }
+
+    public String description() {
+        return this.description;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+}
