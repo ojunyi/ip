349953d00a168d9d35c947d0bf0b2f5ae18bcf8d
JY <junyi4446@gmail.com>
2026-01-22 14:04:38 +0800

    Added: Error Handling, Methods for Todo/Deadline/Event | Updated: UI

--- src/main/java/BuddiBoi.java
+++ src/main/java/BuddiBoi.java
@@ -1,107 +1,186 @@
 import java.util.Scanner;
 
 public class BuddiBoi {
+
+    static Task[] tasks = new Task[100];
+
     public static void main(String[] args) {
-        String divider = "============================================================\n";
-        String indent = "    < ";
-        String logo = indent + "______           _     _ _______       _ \n"
-                    + indent + "| ___ \\         | |   | (_) ___ \\     (_)\n"
-                    + indent + "| |_/ /_   _  __| | __| |_| |_/ / ___  _ \n"
-                    + indent + "| ___ \\ | | |/ _` |/ _` | | ___ \\/ _ \\| |\n"
-                    + indent + "| |_/ / |_| | (_| | (_| | | |_/ / (_) | |\n"
-                    + indent + "\\____/ \\__,_|\\__,_|\\__,_|_\\____/ \\___/|_|\n";
-        
-        
+        String logo = "______           _     _ _______       _ \n"
+                    + "| ___ \\         | |   | (_) ___ \\     (_)\n"
+                    + "| |_/ /_   _  __| | __| |_| |_/ / ___  _ \n"
+                    + "| ___ \\ | | |/ _` |/ _` | | ___ \\/ _ \\| |\n"
+                    + "| |_/ / |_| | (_| | (_| | | |_/ / (_) | |\n"
+                    + "\\____/ \\__,_|\\__,_|\\__,_|_\\____/ \\___/|_|\n";
+
         Scanner sc = new Scanner(System.in);
-        Task[] tasks = new Task[100]; // No more than 100 Tasks
         int itemCount = 0;
 
-        System.out.println(divider);
-        System.out.println(indent + "Hey, whats up! I am\n" + logo);
-        System.out.println(indent + "What can I do for you today?");
-        System.out.println("\n" + divider);
+        returnText("Hey, whats up! I am\n" + logo + "\nWhat can I do for you today?");
 
         while (true) { 
             String input = sc.nextLine();
             System.out.println("> Command: " + input);
 
             if (input.equals("bye")) {
-                System.out.println(divider);
-                System.out.println(indent + "See you next timeee! Ciaoooo!");
-                System.out.println("\n" + divider);
+                goodBye();
                 break;
 
             } else if (input.equals("list")) {
-                System.out.println(divider);
-                System.out.println(indent + "Here's your tasks lists:");
-                for (int i = 0; i < itemCount; i++) {
-                    System.out.println(indent + (i + 1) + ". " + tasks[i].toString());
-                }
-                System.out.println("\n" + divider);
+                listTasks(itemCount);
 
             } else if (input.startsWith("mark ")) {
-                int taskNumber = Integer.parseInt(input.substring(5)) - 1;
-                tasks[taskNumber].markAsDone();
-                System.out.println(divider);
-                System.out.println(indent + "Alright! Done that task shall be:");
-                System.out.println(indent + "  " + tasks[taskNumber].getStatus() + " " + tasks[taskNumber].description());
-                System.out.println("\n" + divider);
+                String taskNumberStr = input.substring(5).trim();
 
+                if (taskNumberStr.matches("\\d+") && Integer.parseInt(taskNumberStr) - 1 < itemCount) {
+                    int taskNumber = Integer.parseInt(taskNumberStr) - 1;
+                    markTask(taskNumber);
+                } else {
+                    // Error handling for invalid task number
+                    returnText("Invalid task number due to possible reasons:\n"
+                                + " - Empty input\n"
+                                + " - Not a number\n"
+                                + " - Number exceeds total tasks\n"
+                                + "Please use - mark <task number>"
+                    );
+                }
+                
             } else if (input.startsWith("unmark ")) {
-                int taskNumber = Integer.parseInt(input.substring(7)) - 1;
-                tasks[taskNumber].markAsNotDone();
-                System.out.println(divider);
-                System.out.println(indent + "OK. Task shall be undone:");
-                System.out.println(indent + "  " + tasks[taskNumber].getStatus() + " " + tasks[taskNumber].description());
-                System.out.println("\n" + divider);
+                String taskNumberStr = input.substring(7).trim();
+
+                if (taskNumberStr.matches("\\d+") && Integer.parseInt(taskNumberStr) - 1 < itemCount) {
+                    int taskNumber = Integer.parseInt(taskNumberStr) - 1;
+                    unMarkTask(taskNumber);
+                } else {
+                    // Error handling for invalid task number
+                    returnText("Invalid task number due to possible reasons:\n"
+                                + " - Empty input\n"
+                                + " - Not a number\n"
+                                + " - Number exceeds total tasks\n"
+                                + "Please use - unmark <task number>"
+                    );
+                }
 
             } else if (input.startsWith("todo ")) {
                 String description = input.substring(5);
-                Task todoTask = new Todos(description);
                 
-                tasks[itemCount] = todoTask;
-                itemCount++;
+                if (!description.isBlank()) {
+                    Task todoTask = new Todos(description);
+                    tasks[itemCount] = todoTask;
+                    itemCount++;
 
-                System.out.println(divider);
-                System.out.println(indent + "Added: " + todoTask.toString());
-                System.out.println("\n" + divider);
+                    returnText("Added: " + todoTask.toString());
+
+                } else {
+                    // Error handling for invalid task number
+                    returnText("Description cannot be empty.\n"
+                                + "Please use - todo <description>"
+                    );
+                }
 
             } else if (input.startsWith("deadline ")) {
                 String[] parts = input.substring(9).split(" /by ");
-                String description = parts[0];
-                String by = parts[1];
-                Task deadlineTask = new Deadline(description, by);
-                
-                tasks[itemCount] = deadlineTask;
-                itemCount++;
 
-                System.out.println(divider);
-                System.out.println(indent + "Added: " + deadlineTask.toString());
-                System.out.println("\n" + divider);
+                if (parts.length == 2) {
+                    String description = parts[0];
+                    String by = parts[1];
+
+                    Task deadlineTask = new Deadline(description, by);
+                
+                    tasks[itemCount] = deadlineTask;
+                    itemCount++;
+
+                    returnText("Added: " + deadlineTask.toString());
+                } else {
+                    // Error handling for invalid deadline format
+                    returnText("Invalid deadline format due to possible reasons:\n"
+                                + " - No /by was found\n"
+                                + " - More than one /by was found\n"
+                                + "Please use - deadline <description> /by <deadline>"
+                    );
+                }
 
             } else if (input.startsWith("event ")) {
                 String[] part_1 = input.substring(6).split(" /from ");
-                String description = part_1[0];
-                String[] part_2 = part_1[1].split(" /to ");
-                String from = part_2[0];
-                String to = part_2[1];
-                Task eventTask = new Events(description, from, to);
-                
-                tasks[itemCount] = eventTask;
-                itemCount++;
 
-                System.out.println(divider);
-                System.out.println(indent + "Added: " + eventTask.toString());
-                System.out.println("\n" + divider);
-                
+                if (part_1.length == 2) {
+                    String description = part_1[0];
+                    String[] part_2 = part_1[1].split(" /to ");
+
+                    if (part_2.length == 2) {
+                        String from = part_2[0];
+                        String to = part_2[1];
+                        Task eventTask = new Events(description, from, to);
+                        
+                        tasks[itemCount] = eventTask;
+                        itemCount++;
+
+                        returnText("Added: " + eventTask.toString());
+                    } else {
+                        // Error handling for invalid event format
+                        returnText("Invalid event format due to possible reasons:\n"
+                                    + " - No /to was found\n"
+                                    + " - More than one /to was found\n"
+                                    + "Please use - event <description> /from <start date> /to <end date>"
+                        );
+                    }
+                } else {
+                    // Error handling for invalid event format
+                    returnText("Invalid event format due to possible reasons:\n"
+                                + " - No /from was found\n"
+                                + " - More than one /from was found\n"
+                                + " - /from and /to are in the wrong order\n"
+                                + "Please use - event <description> /from <start date> /to <end date>"
+                    );
+                }
+
             } else {
-                System.out.println(divider);
-                System.out.println(indent + "Echo: " + input);
-                System.out.println("\n" + divider);
+                returnText("Echo: " + input + " ~~~");
             }
-
         }
 
         sc.close();
+    }
+
+    public static void returnText(String input) {
+        String divider = "============================================================\n";
+        String indent = "    < ";
+
+        System.out.println(divider);
+        for (String line : input.split("\n")) {
+            System.out.println(indent + line.trim());
+        }
+        System.out.println("\n" + divider);
+    }
+
+    public static void goodBye() {
+        returnText("See you next timeee! Ciaoooo ~~~");
+    }
+
+    public static void listTasks(int itemCount) {
+        String str = "Here's your tasks lists:\n";
+        for (int i = 0; i < itemCount; i++) {
+            str = str
+                + (i + 1)
+                + ". "
+                + tasks[i].toString() + "\n";
+        }
+
+        returnText(str);
+    }
+
+    public static void markTask(int taskNumber) {
+        tasks[taskNumber].markAsDone();
+        returnText("Alright! Done that task shall be:\n  "
+                    + tasks[taskNumber].getStatus()
+                    + " "
+                    + tasks[taskNumber].description());
+    }
+
+    public static void unMarkTask(int taskNumber) {
+        tasks[taskNumber].markAsNotDone();
+        returnText("OK. Task shall be undone:  "
+                    + tasks[taskNumber].getStatus()
+                    + " "
+                    + tasks[taskNumber].description());
     }
 }
