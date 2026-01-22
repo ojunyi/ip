ab084521362099ca86fc6be62836cdd1c57dc561
JY <junyi4446@gmail.com>
2026-01-22 14:41:59 +0800

    Added: Delete, More error handling | Updated: Changed Array to ArrayList

--- src/main/java/BuddiBoi.java
+++ src/main/java/BuddiBoi.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BuddiBoi {
 
-    static Task[] tasks = new Task[100];
+    static ArrayList<Task> tasks = new ArrayList<>();
+    static int itemCount = 0;
 
     public static void main(String[] args) {
         String logo = "______           _     _ _______       _ \n"
@@ -13,8 +15,7 @@ public class BuddiBoi {
                     + "\\____/ \\__,_|\\__,_|\\__,_|_\\____/ \\___/|_|\n";
 
         Scanner sc = new Scanner(System.in);
-        int itemCount = 0;
-
+        
         returnText("Hey, whats up! I am\n" + logo + "\nWhat can I do for you today?");
 
         while (true) { 
@@ -36,7 +37,7 @@ public class BuddiBoi {
                     markTask(taskNumber);
                 } else {
                     // Error handling for invalid task number
-                    returnText("Invalid task number due to possible reasons:\n"
+                    returnText("Invalid command due to possible reasons:\n"
                                 + " - Empty input\n"
                                 + " - Not a number\n"
                                 + " - Number exceeds total tasks\n"
@@ -52,7 +53,7 @@ public class BuddiBoi {
                     unMarkTask(taskNumber);
                 } else {
                     // Error handling for invalid task number
-                    returnText("Invalid task number due to possible reasons:\n"
+                    returnText("Invalid command due to possible reasons:\n"
                                 + " - Empty input\n"
                                 + " - Not a number\n"
                                 + " - Number exceeds total tasks\n"
@@ -65,14 +66,14 @@ public class BuddiBoi {
                 
                 if (!description.isBlank()) {
                     Task todoTask = new Todos(description);
-                    tasks[itemCount] = todoTask;
+                    tasks.add(todoTask);
                     itemCount++;
 
                     returnText("Added: " + todoTask.toString());
 
                 } else {
                     // Error handling for invalid task number
-                    returnText("Description cannot be empty.\n"
+                    returnText("Invalid command due to possible reasons:\n"
                                 + "Please use - todo <description>"
                     );
                 }
@@ -86,13 +87,13 @@ public class BuddiBoi {
 
                     Task deadlineTask = new Deadline(description, by);
                 
-                    tasks[itemCount] = deadlineTask;
+                    tasks.add(deadlineTask);
                     itemCount++;
 
                     returnText("Added: " + deadlineTask.toString());
                 } else {
                     // Error handling for invalid deadline format
-                    returnText("Invalid deadline format due to possible reasons:\n"
+                    returnText("Invalid command due to possible reasons:\n"
                                 + " - No /by was found\n"
                                 + " - More than one /by was found\n"
                                 + "Please use - deadline <description> /by <deadline>"
@@ -110,14 +111,14 @@ public class BuddiBoi {
                         String from = part_2[0];
                         String to = part_2[1];
                         Task eventTask = new Events(description, from, to);
-                        
-                        tasks[itemCount] = eventTask;
+
+                        tasks.add(eventTask);
                         itemCount++;
 
                         returnText("Added: " + eventTask.toString());
                     } else {
                         // Error handling for invalid event format
-                        returnText("Invalid event format due to possible reasons:\n"
+                        returnText("Invalid command due to possible reasons:\n"
                                     + " - No /to was found\n"
                                     + " - More than one /to was found\n"
                                     + "Please use - event <description> /from <start date> /to <end date>"
@@ -125,11 +126,28 @@ public class BuddiBoi {
                     }
                 } else {
                     // Error handling for invalid event format
-                    returnText("Invalid event format due to possible reasons:\n"
+                    returnText("Invalid command due to possible reasons:\n"
                                 + " - No /from was found\n"
                                 + " - More than one /from was found\n"
                                 + " - /from and /to are in the wrong order\n"
                                 + "Please use - event <description> /from <start date> /to <end date>"
+                    );
+                }
+            } else if (input.startsWith("delete ")) {
+                String taskNumberStr = input.substring(7).trim();
+
+                if (taskNumberStr.matches("\\d+") && Integer.parseInt(taskNumberStr) - 1 < itemCount) {
+                    int taskNumber = Integer.parseInt(taskNumberStr) - 1;
+                    
+                    delete(taskNumber);
+                    
+                } else {
+                    // Error handling for invalid task number
+                    returnText("Invalid command due to possible reasons:\n"
+                                + " - Empty input\n"
+                                + " - Not a number\n"
+                                + " - Number exceeds total tasks\n"
+                                + "Please use - delete <task number>"
                     );
                 }
 
@@ -147,7 +165,7 @@ public class BuddiBoi {
 
         System.out.println(divider);
         for (String line : input.split("\n")) {
-            System.out.println(indent + line.trim());
+            System.out.println(indent + line.replaceAll("\\s+$", ""));
         }
         System.out.println("\n" + divider);
     }
@@ -162,25 +180,32 @@ public class BuddiBoi {
             str = str
                 + (i + 1)
                 + ". "
-                + tasks[i].toString() + "\n";
+                + tasks.get(i).toString() + "\n";
         }
 
         returnText(str);
     }
 
     public static void markTask(int taskNumber) {
-        tasks[taskNumber].markAsDone();
-        returnText("Alright! Done that task shall be:\n  "
-                    + tasks[taskNumber].getStatus()
-                    + " "
-                    + tasks[taskNumber].description());
+        Task task = tasks.get(taskNumber);
+        task.markAsDone();
+        returnText("Alrightyy! Congratz on completing that task!:\n"
+                    + "  " + task.toString());
     }
 
     public static void unMarkTask(int taskNumber) {
-        tasks[taskNumber].markAsNotDone();
-        returnText("OK. Task shall be undone:  "
-                    + tasks[taskNumber].getStatus()
-                    + " "
-                    + tasks[taskNumber].description());
+        Task task = tasks.get(taskNumber);
+        task.markAsNotDone();
+        returnText("Okay. Task shall be undone:\n"
+                    + "  " + task.toString());
+    }
+
+    public static void delete(int taskNumber) {
+        Task task = tasks.get(taskNumber);
+        returnText("Noted. I've removed this task. Less work for you:\n"
+                    + "  " + task.toString()
+                    + "\nNow you have " + (itemCount - 1) + " tasks in the list.");
+        tasks.remove(taskNumber);
+        itemCount--;
     }
 }
