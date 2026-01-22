40655beb01ce7fa5788da945cc72a8720e80151b
JY <junyi4446@gmail.com>
2026-01-22 10:33:07 +0800

    Added: Echo

--- src/main/java/BuddiBoi.java
+++ src/main/java/BuddiBoi.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class BuddiBoi {
     public static void main(String[] args) {
         String logo = "______           _     _ _______       _ \n"
@@ -6,9 +8,31 @@ public class BuddiBoi {
                     + "| ___ \\ | | |/ _` |/ _` | | ___ \\/ _ \\| |\n"
                     + "| |_/ / |_| | (_| | (_| | | |_/ / (_) | |\n"
                     + "\\____/ \\__,_|\\__,_|\\__,_|_\\____/ \\___/|_|\n";
+        String divider = "============================================================\n";
+        
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Hello! I am\n" + logo);
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(divider);
+        System.out.println("Hiiii! I am\n" + logo);
+        System.out.println("What can I do for you today?");
+        System.out.println("\n" + divider);
+
+        while (true) { 
+            String input = sc.nextLine();
+            System.out.println("Command: " + input);
+
+            if (input.equals("bye")) {
+                System.out.println(divider);
+                System.out.println("See you next timeee!");
+                System.out.println("\n" + divider);
+                break;
+            }
+
+            System.out.println(divider);
+            System.out.println(input);
+            System.out.println("\n" + divider);
+        }
+
+        sc.close();
     }
 }
