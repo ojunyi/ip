c427d6b7197f2eb7e0f5822fd4e55e44fe93c807
JY <junyi4446@gmail.com>
2026-01-29 11:25:47 +0800

    Add Ui class

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,9 +1,9 @@
-public class Events extends Task {
+public class Event extends Task {
 
     protected String startDate;
     protected String endDate;
 
-    public Events(String description, String startDate, String endDate) {
+    public Event(String description, String startDate, String endDate) {
         super(description);
         this.startDate = startDate;
         this.endDate = endDate;
