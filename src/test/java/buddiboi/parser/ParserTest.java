480536540cfc3d1b8907cb0f3b6a94b901dd7373
JY <junyi4446@gmail.com>
2026-02-04 12:18:45 +0800

    Add JUnit tests ParseCommandTest.java and ParserTest.java

---
+++ src/test/java/buddiboi/parser/ParserTest.java
@@ -0,0 +1,34 @@
+package buddiboi.parser;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class ParserTest {
+    
+    @Test
+    public void parse_emptyString_returnsEmptyCommand() {
+        String[] result = Parser.parse("");
+        assertArrayEquals(new String[] {""}, result);
+    }
+    
+    @Test
+    public void parse_singleWord_returnsSingleElement() {
+        String[] result = Parser.parse("list");
+        assertArrayEquals(new String[] {"list"}, result);
+    }
+    
+    @Test
+    public void parse_commandWithArgs_returnsTwoElements() {
+        String[] result = Parser.parse("todo read book");
+        assertEquals(2, result.length);
+        assertEquals("todo", result[0]);
+        assertEquals("read book", result[1]);
+    }
+
+    @Test
+    public void parse_extraSpaces_trimsCorrectly() {
+        String[] result = Parser.parse("  deadline  return book  ");
+        assertArrayEquals(new String[] {"deadline", "return book"}, result);
+    }
+}
